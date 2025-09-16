755ae1a81d168cc621551ce3c1c14e4c8985bc41
csalguera <carlos.e.salguera@gmail.com>
2025-09-16 14:30:10 -0400

    import react-native components and add styling

--- todo-list/App.js
+++ todo-list/App.js
@@ -1,9 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>To-Do List</Text>
+      <View style={styles.inputRow}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add To-Do"
+        />
+        <Button title="Add" />
+      </View>
     </View>
   );
 }
@@ -11,7 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 20,
     paddingTop: 60,
   },
@@ -20,5 +33,18 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 15,
     textAlign: "center",
+  },
+  inputRow: {
+    flexDirection: "row",
+    marginBottom: 15,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 8,
+    marginRight: 10,
+    borderRadius: 5,
+    backgroundColor: "#f9f9f9",
   },
 });
